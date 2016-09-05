46f7309707d7c97429f8e967445d377cefad4227
James Hall <gymboh32@gmail.com>
2016-09-05 17:08:51 -0400

    Created main Fragment

---
+++ app/src/main/java/org/ragecastle/firebasesample/MainFragment.java
@@ -0,0 +1,14 @@
+package org.ragecastle.firebasesample;
+
+import android.app.Fragment;
+
+/**
+ * Created by jahall on 9/5/16.
+ *
+ *
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() { }
+
+}
