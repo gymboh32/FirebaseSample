820bdd19a2410457a19dde08c8d506d0685e7572
James Hall <gymboh32@gmail.com>
2016-09-06 20:30:07 -0400

    Setup a simple push and pull fromt he firebase database

--- app/src/main/java/org/ragecastle/firebasesample/MainFragment.java
+++ app/src/main/java/org/ragecastle/firebasesample/MainFragment.java
@@ -3,10 +3,17 @@ package org.ragecastle.firebasesample;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
@@ -17,6 +24,9 @@ import java.util.Arrays;
  */
 public class MainFragment extends Fragment {
 
+    private static final String LOG_TAG = MainFragment.class.getSimpleName();
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     public MainFragment() { }
 
     @Nullable
@@ -25,6 +35,25 @@ public class MainFragment extends Fragment {
                              ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        Item item = new Item("Item_2");
+        databaseReference.push().setValue(item);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Item item1 = dataSnapshot1.getValue(Item.class);
+                    Log.e(LOG_TAG, item1.name());
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e (LOG_TAG, "Shits Broke: " + databaseError.getDetails());
+            }
+        });
 
         String [] itemsList = {
                 "Item_1",
